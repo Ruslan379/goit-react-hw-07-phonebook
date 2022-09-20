26c60212c46cd75868ef294a3b186feb0238b069
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-09-21 02:02:19 +0300

    WORK 100% (with RTK Query) 35

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'; //! +++
+// import { useEffect, useState } from 'react'; //! +++
 import { useDispatch, useSelector } from "react-redux"; //! +++
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -6,13 +6,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // import { store } from 'redux/store'; //? +++ 
 
-import * as itemsOperations from 'redux/items/itemsOperations';
+// import * as itemsOperations from 'redux/items/itemsOperations'; //! +-+-+-+-
 // import { itemsOperations } from 'redux'; //! ТАК НЕ РАБОТАЕТ с Re-export
 
-// import { getContacts } from 'redux/items/itemsSelectors';
+// import { getContacts } from 'redux/items/itemsSelectors'; //! +-+-+-+-
 import { getFilter } from 'redux/filter/filterSelectors';
-// import { getIsLoading } from 'redux/isLoading/isLoadingSelectors';
-import { getError } from 'redux/error/errorSelectors';
+// import { getIsLoading } from 'redux/isLoading/isLoadingSelectors'; //! +-+-+-+-
+// import { getError } from 'redux/error/errorSelectors';
 
 // import { itemsSelectors, filterSelectors } from 'redux'; //! ТАК НЕ РАБОТАЕТ с Re-export
 
@@ -33,7 +33,7 @@ import { useGetAddAllContactsQuery, usePostAddContactMutation, useDeleteContactM
 
 export const App = () => {
   //! useState ===> contacts (аналог this.state.contacts)
-  const [contacts, setContacts] = useState([]); 
+  // const [contacts, setContacts] = useState([]); 
   // const [isLoading, setIsLoading] = useState(false);
 
   //! +++++++ Хук useDispatch +++++++++++++
@@ -54,7 +54,7 @@ export const App = () => {
   // const isLoading = useSelector(getIsLoading); //! +-+-+-+-
   // console.log("isLoading:", isLoading); //!
 
-  const error = useSelector(getError); //! +-+-+-+-
+  // const error = useSelector(getError); //! +-+-+-+-
   // console.log("error:", error); //!
 
   //? +++++++++++ with RTK Query & pokemon.js +++++++++++++++ НЕ РАБОТАЕТ!!!
@@ -72,61 +72,61 @@ export const App = () => {
   // console.log("dataRTKQuery:", dataRTKQuery); //!
   // console.log("errorPokemonRTKQuery:", errorPokemonRTKQuery); //!
   // console.log("isLoadingPokemonRTKQuery:", isLoadingPokemonRTKQuery); //!
-//--------------------------------------------------------------------------------
+//----------------------------------- GET ------------------------------------------
   // const { data: contacts, isFetching: isLoading, error: error} = usegetAddAllContactsQuery();
-  const { data = [], isFetching: isLoading, } = useGetAddAllContactsQuery();
+  const { data = [], isFetching: isLoading, error} = useGetAddAllContactsQuery();
   // console.log("contacts_RTK:", contacts); //!
   console.log("data:", data); //!
   console.log("isLoading_RTK:", isLoading); //!
   // const contacts = data; //! +-+-+-+-
   // console.log("error_RTK:", error_RTK); //!
 
-  useEffect(() => {
-    const addAllСontact =  async (data) => {
-      const newIdItems = await data.map(item => {
+  // useEffect(() => {
+    // const addAllСontact =  async (data) => {
+      const contacts = data.map(item => {
         return {
           id: item.id,
           name: item.name,
           number: item.phone
         };
       });
-      setContacts(newIdItems);
-    };
+      // setContacts(newIdItems);
+    // };
 
-    addAllСontact(data);
-  }, [data]);
+    // addAllСontact(data);
+  // }, [data]);
 
 console.log("contacts_RTK:", contacts); //!
 
-//--------------------------------------------------------------------------------
+//--------------------------------- POST ---------------------------------------
   const [addContact, { isError }] = usePostAddContactMutation();
   console.log("isError:", isError); //!
 
-  const handleAddContact = async () => {
-    // const addNewContact = { name, phone: number };
-    const addNewContact1 = { name: "Polly", phone: "111-11-111" };
-    if (addNewContact1) {
-      await addContact(addNewContact1).unwrap()
-    };
-  };
+  // const handleAddContact = async () => {
+  //   // const addNewContact = { name, phone: number };
+  //   const addNewContact1 = { name: "Polly", phone: "111-11-111" };
+  //   if (addNewContact1) {
+  //     await addContact(addNewContact1).unwrap()
+  //   };
+  // };
 
-  console.log("handleAddContact:", handleAddContact); //!
+  // console.log("handleAddContact:", handleAddContact); //!
 
 
-  //--------------------------------------------------------------------------------
+  //------------------------------------ DELETE --------------------------------------------
   const [deleteContact] = useDeleteContactMutation();
   
-  const handleDeleteContact = async (id) => {
-      await deleteContact(id).unwrap()
-  };
+  // const handleDeleteContact = async (id) => {
+  //     await deleteContact(id).unwrap()
+  // };
 
-  console.log("handleDeleteContact:", handleDeleteContact); //!
+  // console.log("handleDeleteContact:", handleDeleteContact); //!
 //?________________________________________________
 
   
   
   
-  
+  //? уже не надо с RTK Query
   //! Добавление ALL Contacts с помощью axios.get-запроса 
   // useEffect(() => dispatch(itemsOperations.addAllContactsFromMmockapiIo()), [dispatch]);  //! ТАК НЕ РАБОТАЕТ!!!
 
@@ -134,24 +134,11 @@ console.log("contacts_RTK:", contacts); //!
   //   dispatch(itemsOperations.addAllContactsFromMmockapiIo());
   // }, [dispatch]);
 
-  
+
+
   //! Принимаем (name, number) из ContactForm
   //! alert с предупреждением о наявности контакта
   //!  Добавление контакта в Действия (actions) ==> 
-  // const formSubmitHandler = (name, number) => {
-  //   if (contacts.find(item => item.name.toLowerCase() === name.toLowerCase())) {
-  //     toast.warning(`${name} уже есть в контактах.`); 
-  //     return;
-  //   } else {
-  //     const addNewContact = { name, phone: number };
-  //     //! Делаем запрос на добавление контакта из mockapi.io/contacts 
-  //     dispatch(itemsOperations.addOneContactToMmockapiIo(addNewContact));
-  //     }
-  // };
-
-  //? Принимаем (name, number) из ContactForm
-  //? alert с предупреждением о наявности контакта
-  //?  Добавление контакта в Действия (actions) ==> 
   const formSubmitHandler = (name, number) => {
     if (contacts.find(item => item.name.toLowerCase() === name.toLowerCase())) {
       toast.warning(`${name} уже есть в контактах.`); 
@@ -159,7 +146,8 @@ console.log("contacts_RTK:", contacts); //!
     } else {
       const addNewContact = { name, phone: number };
       //! Делаем запрос на добавление контакта из mockapi.io/contacts 
-      dispatch(itemsOperations.addOneContactToMmockapiIo(addNewContact));
+      // dispatch(itemsOperations.addOneContactToMmockapiIo(addNewContact)); //? уже не надо с RTK Query
+      addContact(addNewContact).unwrap()
       }
   };
 
@@ -177,7 +165,8 @@ console.log("contacts_RTK:", contacts); //!
 //! Создание нового массива объектов из this.state.contacts с учетом удаления контакта по его contact.id
   const deleteTodo = contactId => {
     //! Делаем запрос на УДАЛЕНИЕ контакта из mockapi.io/contacts 
-    dispatch(itemsOperations.deleteOneContactFromMmockapiIo(contactId));
+    // dispatch(itemsOperations.deleteOneContactFromMmockapiIo(contactId)); //? уже не надо с RTK Query
+    deleteContact(contactId).unwrap()
   };
 
 
@@ -192,9 +181,9 @@ console.log("contacts_RTK:", contacts); //!
   };
 
 
-  // const visibleContacts = contacts; //! временно
+  // const visibleContacts = contacts; //? временно, для RTK Query
   const visibleContacts = getVisibleContacts();
-  // const totalContacts = 1; //! временно
+  // const totalContacts = 1; //? временно, для RTK Query
   const totalContacts = contacts.length;
 
 
@@ -208,7 +197,7 @@ console.log("contacts_RTK:", contacts); //!
       <Container>
         <ToastContainer autoClose={1500} theme={"colored"} />
 
-        <h1>Phonebook HW-7<span style={{ fontSize: "20px" }}> (with createAsyncThunk)</span></h1>
+        <h1>Phonebook HW-7<span style={{ fontSize: "20px" }}> (with RTK Query)</span></h1>
 
         <ContactForm onSubmit={formSubmitHandler} />
 
@@ -218,7 +207,7 @@ console.log("contacts_RTK:", contacts); //!
       {error && (
           <div style={{ margin: '0 auto', color: 'red' }}>
             <h1>The request failed:</h1>
-            <h2 style={{ textDecoration: "underline", fontStyle: 'italic', color: '#a10000' }}>!!! {error}</h2>
+            <h2 style={{ textDecoration: "underline", fontStyle: 'italic', color: '#a10000' }}>!!! {error.data}</h2>
           </div>
         )}
 
@@ -231,7 +220,7 @@ console.log("contacts_RTK:", contacts); //!
         {isLoading && <Loader />}
         <br/>
 
-        {/* //? +++++++++++ with RTK Query +++++++++++++++ */}
+        {/* //? ++++++ Временно - для проверки data with RTK Query +++++ */}
         {/* <p>Contacts with RTK Query</p>
         <ul>
           {data.map(({ id, name, phone }) => (

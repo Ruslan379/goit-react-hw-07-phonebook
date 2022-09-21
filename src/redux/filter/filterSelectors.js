d3c70ff6c7d0b0931986bcf064a5c5ba264d5de1
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-09-21 03:31:05 +0300

    WORK 100% (with RTK Query) (fixed bug in reducers & store) 37

--- src/redux/filter/filterSelectors.js
+++ src/redux/filter/filterSelectors.js
@@ -1,1 +1,2 @@
-export const getFilter = state => state.contacts.filter;
+// export const getFilter = state => state.contacts.filter;
+export const getFilter = state => state.filter; //! для 3-го варианта store with RTK Query
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux'; //? уже не надо с RTK Query
 
-// import { itemsSlice } from 'redux/items/itemsSlice'; //! +-+-+-+-
+// import { itemsSlice } from 'redux/items/itemsSlice'; //? уже не надо с RTK Query
 import { filterSlice } from 'redux/filter/filterSlice';
-// import { isLoadingSlice } from 'redux/isLoading/isLoadingSlice'; //! +-+-+-+-
-// import { errorSlice } from 'redux/error/errorSlice'; //! +-+-+-+-
+// import { isLoadingSlice } from 'redux/isLoading/isLoadingSlice'; //? уже не надо с RTK Query
+// import { errorSlice } from 'redux/error/errorSlice'; //? уже не надо с RTK Query
 
 
 //? +++++++++++ with RTK Query +++++++++++++++
@@ -31,7 +31,7 @@ import { itemsAPIbyRTKQuery } from 'services/mockapi_io-api';
 //todo_________________________________________________________________________
 
 
-// //! With createSlice
+//! With createSlice
 // const rootReducer = combineReducers({
 //     items: itemsSlice.reducer,
 //     filter: filterSlice.reducer,
@@ -40,13 +40,14 @@ import { itemsAPIbyRTKQuery } from 'services/mockapi_io-api';
 // });
 
 //? +++++++++++ store with with RTK Query pokemon.js (1 вариант - РАБОЧИЙ) +++++++++++++++
-const rootReducer = combineReducers({
-    // items: itemsSlice.reducer,
-    // [itemsAPIbyRTKQuery.reducerPath]: itemsAPIbyRTKQuery.reducer,
-    filter: filterSlice.reducer,
-    // isLoading: isLoadingSlice.reducer,
-    // error: errorSlice.reducer
-});
+//? export const getFilter = state => state.contacts.filter;
+// const rootReducer = combineReducers({
+//     // items: itemsSlice.reducer,
+//     // [itemsAPIbyRTKQuery.reducerPath]: itemsAPIbyRTKQuery.reducer,
+//     filter: filterSlice.reducer,
+//     // isLoading: isLoadingSlice.reducer,
+//     // error: errorSlice.reducer
+// });
 
 
 //! +++++++++++ store +++++++++++++++
@@ -57,14 +58,15 @@ const rootReducer = combineReducers({
 // });
 
 //? +++++++++++ store with RTK Query & pokemon.js  (1 вариант - РАБОЧИЙ) +++++++++++++++
-export const store = configureStore({
-    reducer: {
-        contacts: rootReducer,
-        // [pokemonApi.reducerPath]: pokemonApi.reducer,
-        [itemsAPIbyRTKQuery.reducerPath]: itemsAPIbyRTKQuery.reducer,
-    },
-    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), itemsAPIbyRTKQuery.middleware],
-});
+//? export const getFilter = state => state.contacts.filter;
+// export const store = configureStore({
+//     reducer: {
+//         contacts: rootReducer,
+//         // [pokemonApi.reducerPath]: pokemonApi.reducer,
+//         [itemsAPIbyRTKQuery.reducerPath]: itemsAPIbyRTKQuery.reducer,
+//     },
+//     middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), itemsAPIbyRTKQuery.middleware],
+// });
 
 
 //? +++++++++++ store with RTK Query (2 вариант - НЕ РАБОЧИЙ, зависает на Loader) +++++++++++++++
@@ -81,7 +83,15 @@ export const store = configureStore({
 //     middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), itemsAPIbyRTKQuery.middleware],
 // });
 
-
+//! +++++++++++ store with RTK Query (3-й вариант - РАБОТАЕТ!!!) +++++++++++++++
+//! export const getFilter = state => state.filter;
+export const store = configureStore({
+    reducer: {
+        [itemsAPIbyRTKQuery.reducerPath]: itemsAPIbyRTKQuery.reducer,
+        filter: filterSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), itemsAPIbyRTKQuery.middleware],
+});
 
 //! ++++++++++++++++++++++++++++ ВЕСЬ State +++++++++++++++++++++++++++++++++++
 // console.log("ВЕСЬ State из store.js ==> store.getState():", store.getState()); //!
